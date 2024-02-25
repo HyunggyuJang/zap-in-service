11d5c720f12570d7ce395d252e44470d884dd1b6
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-02-26 00:07:07 +0900

    Introduce lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,18 @@
 import 'dotenv/config';
-import { HardhatUserConfig } from "hardhat/config";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@openzeppelin/hardhat-upgrades';
+import '@nomicfoundation/hardhat-toolbox';
 
 const mainnetFork = {
   url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
   blockNumber: parseInt(process.env.BLOCK_NUMBER ?? '0') || 19304000,
-}
+};
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: '0.8.20',
     settings: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       viaIR: true,
       optimizer: {
         enabled: true,
--- test/Helper.test.ts
+++ test/Helper.test.ts
@@ -1,48 +1,51 @@
 import {
   time,
   loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { expect } from "chai";
-import { ethers, upgrades } from "hardhat";
-import { parseEther } from "ethers";
+} from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { expect } from 'chai';
+import { ethers, upgrades } from 'hardhat';
+import { parseEther } from 'ethers';
 import {
   Helper,
   IUniswapV2Factory__factory,
   IUniswapV2Pair__factory,
   IUniswapV2Router__factory,
   TestERC20__factory,
-} from "../types";
+} from '../types';
 
-describe("Helper", function () {
-  const MAINNET_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+describe('Helper', function () {
+  const MAINNET_ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async function setup() {
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Helper = await ethers.getContractFactory("Helper");
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const Helper = await ethers.getContractFactory('Helper');
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const _helper = await upgrades.deployProxy(Helper, [
       MAINNET_ROUTER_ADDRESS,
     ]);
     await _helper.waitForDeployment();
     const helper = _helper as unknown as Helper;
 
-    const testTokenInitialSupply = parseEther("1000000");
-    const testTokenDepositAmount = parseEther("1000");
+    const testTokenInitialSupply = parseEther('1000000');
+    const testTokenDepositAmount = parseEther('1000');
     const mat = await new TestERC20__factory(owner).deploy(
-      "MAT",
-      "MAT",
-      testTokenInitialSupply
+      'MAT',
+      'MAT',
+      testTokenInitialSupply,
     );
     const mbt = await new TestERC20__factory(owner).deploy(
-      "MBT",
-      "MBT",
-      testTokenInitialSupply
+      'MBT',
+      'MBT',
+      testTokenInitialSupply,
     );
     await mat.waitForDeployment();
     await mbt.waitForDeployment();
 
     const router = IUniswapV2Router__factory.connect(
       MAINNET_ROUTER_ADDRESS,
-      owner
+      owner,
     );
 
     await (
@@ -61,76 +64,78 @@ describe("Helper", function () {
         testTokenDepositAmount,
         testTokenDepositAmount,
         owner.address,
-        (await time.latest()) + 1000
+        (await time.latest()) + 1000,
       )
     ).wait();
 
     const factory = IUniswapV2Factory__factory.connect(
       await router.factory(),
-      owner
+      owner,
     );
     const pair = IUniswapV2Pair__factory.connect(
       await factory.getPair(mat.getAddress(), mbt.getAddress()),
-      owner
+      owner,
     );
 
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     return { helper, Helper, pair, mat, mbt, owner, otherAccount };
   }
 
-  describe("Deployment", function () {
-    it("Should be able to deploy", async function () {
+  describe('Deployment', function () {
+    it('Should be able to deploy', async function () {
       const { helper } = await loadFixture(setup);
 
       expect(await helper.uniswapV2Router()).to.equal(MAINNET_ROUTER_ADDRESS);
     });
 
-    it("Should fail if router address is zero", async function () {
+    it('Should fail if router address is zero', async function () {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       const { Helper } = await loadFixture(setup);
 
       await expect(
-        upgrades.deployProxy(Helper, [ethers.ZeroAddress])
-      ).to.be.revertedWith("Invalid router address");
+        upgrades.deployProxy(Helper, [ethers.ZeroAddress]),
+      ).to.be.revertedWith('Invalid router address');
     });
 
-    it("Should fail if try to re-initialize", async function () {
+    it('Should fail if try to re-initialize', async function () {
       const { helper } = await loadFixture(setup);
 
       await expect(
-        helper.initialize(MAINNET_ROUTER_ADDRESS)
-      ).to.be.revertedWithCustomError(helper, "InvalidInitialization");
+        helper.initialize(MAINNET_ROUTER_ADDRESS),
+      ).to.be.revertedWithCustomError(helper, 'InvalidInitialization');
     });
   });
-  describe("singleTokenAddLiquidity", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if pair address is zero", async function () {
+  describe('singleTokenAddLiquidity', function () {
+    describe('Validations', function () {
+      it('Should revert with the right error if pair address is zero', async function () {
         const { helper, mat, otherAccount } = await loadFixture(setup);
 
         await expect(
           helper.singleTokenAddLiquidity(
             ethers.ZeroAddress,
             mat.getAddress(),
-            parseEther("1000"),
+            parseEther('1000'),
             otherAccount.address,
-            (await time.latest()) + 1000
-          )
-        ).to.be.revertedWith("Invalid pair address");
+            (await time.latest()) + 1000,
+          ),
+        ).to.be.revertedWith('Invalid pair address');
       });
 
-      it("Should revert with the right error if token address is zero", async function () {
+      it('Should revert with the right error if token address is zero', async function () {
         const { helper, pair, otherAccount } = await loadFixture(setup);
 
         await expect(
           helper.singleTokenAddLiquidity(
             pair.getAddress(),
             ethers.ZeroAddress,
-            parseEther("1000"),
+            parseEther('1000'),
             otherAccount.address,
-            (await time.latest()) + 1000
-          )
-        ).to.be.revertedWith("Invalid token address");
+            (await time.latest()) + 1000,
+          ),
+        ).to.be.revertedWith('Invalid token address');
       });
 
-      it("Should revert with the right error if token amount is zero", async function () {
+      it('Should revert with the right error if token amount is zero', async function () {
         const { helper, pair, mat, otherAccount } = await loadFixture(setup);
 
         await expect(
@@ -139,46 +144,44 @@ describe("Helper", function () {
             mat.getAddress(),
             0,
             otherAccount.address,
-            (await time.latest()) + 1000
-          )
-        ).to.be.revertedWith("Invalid amount");
+            (await time.latest()) + 1000,
+          ),
+        ).to.be.revertedWith('Invalid amount');
       });
 
-      it("Should revert with the right error if recipient address is zero", async function () {
+      it('Should revert with the right error if recipient address is zero', async function () {
         const { helper, pair, mat } = await loadFixture(setup);
 
         await expect(
           helper.singleTokenAddLiquidity(
             pair.getAddress(),
             mat.getAddress(),
-            parseEther("1000"),
+            parseEther('1000'),
             ethers.ZeroAddress,
-            (await time.latest()) + 1000
-          )
-        ).to.be.revertedWith("Invalid to address");
+            (await time.latest()) + 1000,
+          ),
+        ).to.be.revertedWith('Invalid to address');
       });
 
-      it("Should revert with the right error if deadline has passed", async function () {
+      it('Should revert with the right error if deadline has passed', async function () {
         const { helper, pair, mat, otherAccount } = await loadFixture(setup);
 
         await expect(
           helper.singleTokenAddLiquidity(
             pair.getAddress(),
             mat.getAddress(),
-            parseEther("1000"),
+            parseEther('1000'),
             otherAccount.address,
-            await time.latest()
-          )
-        ).to.be.revertedWith("EXPIRED");
+            await time.latest(),
+          ),
+        ).to.be.revertedWith('EXPIRED');
       });
 
-      it("Should revert with the right error if token amount is too small", async function () {
+      it('Should revert with the right error if token amount is too small', async function () {
         const { helper, pair, mat, otherAccount } = await loadFixture(setup);
 
         // Approve the helper to spend MAT tokens
-        await (
-          await mat.approve(helper.getAddress(), 1)
-        ).wait();
+        await (await mat.approve(helper.getAddress(), 1)).wait();
 
         await expect(
           helper.singleTokenAddLiquidity(
@@ -186,28 +189,28 @@ describe("Helper", function () {
             mat.getAddress(),
             1,
             otherAccount.address,
-            (await time.latest()) + 1000
-          )
-        ).to.be.revertedWith("UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT");
+            (await time.latest()) + 1000,
+          ),
+        ).to.be.revertedWith('UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
       });
     });
 
-    describe("Happy Path", function () {
-      it("Should add liquidity to the pair providing MAT token only", async function () {
+    describe('Happy Path', function () {
+      it('Should add liquidity to the pair providing MAT token only', async function () {
         const { helper, pair, mat, mbt, owner, otherAccount } =
           await loadFixture(setup);
 
         // Approve the helper to spend MAT tokens
         await (
-          await mat.approve(helper.getAddress(), parseEther("1000"))
+          await mat.approve(helper.getAddress(), parseEther('1000'))
         ).wait();
 
         const tx = helper.singleTokenAddLiquidity(
           pair.getAddress(),
           mat.getAddress(),
-          parseEther("1000"),
+          parseEther('1000'),
           otherAccount.address,
-          (await time.latest()) + 1000
+          (await time.latest()) + 1000,
         );
 
         let loggedMinted;
@@ -217,26 +220,26 @@ describe("Helper", function () {
         };
 
         await expect(tx)
-          .to.emit(helper, "ZapIn")
+          .to.emit(helper, 'ZapIn')
           .withArgs(
             owner.address,
             otherAccount.address,
             await pair.getAddress(),
-            positiveWhileLogging
+            positiveWhileLogging,
           );
 
         await expect(tx).to.changeTokenBalances(
           mat,
           [owner, helper],
           [
-            "-999999999999999999997", // very close to parseEther("-1000")
+            '-999999999999999999997', // very close to parseEther("-1000")
             0,
-          ]
+          ],
         ); // consume all the input MAT tokens & helper's doesn't possess any MAT tokens
         await expect(tx).to.changeTokenBalances(mbt, [owner, helper], [0, 0]); // helper's MBT token balance should remain unchanged
 
         expect(await pair.balanceOf(otherAccount.address)).be.equal(
-          loggedMinted
+          loggedMinted,
         ); // LP tokens should be minted and sent to the recipient
       });
     });
