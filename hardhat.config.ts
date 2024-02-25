36397d9d24bd21841fb5577ab00bf6f003f1fbf3
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-02-25 20:58:00 +0900

    Setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
+import 'dotenv/config';
 import { HardhatUserConfig } from "hardhat/config";
+import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 1_000_000,
+      },
+    },
+  },
+  typechain: {
+    outDir: 'types',
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
 };
 
 export default config;
